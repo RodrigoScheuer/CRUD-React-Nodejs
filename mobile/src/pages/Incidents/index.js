b747fadcd625dacfa54f7ca943a2e3410c7d6e94
Scheuer <rodrigo.scheuer@hotmail.com>
2020-03-27 09:30:51 -0300

    create static list of incidents

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './styles';
-import { View, Text, Image } from 'react-native';
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native';
 
+import { Feather } from '@expo/vector-icons';
 import logoImg from '../../assets/logo.png';
 
 export default function Incidents() {
@@ -19,6 +20,32 @@ export default function Incidents() {
             <Text style={styles.description}>
                 Escolha um caso abaixo e salve o dia.
             </Text>
+
+            <FlatList
+                style={styles.incidentList}
+                data={[1, 2, 3]}
+                keyExtractor={(incident) => String(incident)}
+                showsVerticalScrollIndicator={false}
+                renderItem={() => (
+
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}> ONG: </Text>
+                        <Text style={styles.incidentValue}> APAE </Text>
+
+                        <Text style={styles.incidentProperty}> Caso: </Text>
+                        <Text style={styles.incidentValue}> Cachorro Atropelado </Text>
+
+                        <Text style={styles.incidentProperty}> Valor: </Text>
+                        <Text style={styles.incidentValue}> R$ 120,00 </Text>
+
+                        <TouchableOpacity style={styles.detailsButton} onPress={() => { }}>
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#E02041" />
+                        </TouchableOpacity>
+                    </View>
+
+                )}
+            />
         </View>
     )
 }
