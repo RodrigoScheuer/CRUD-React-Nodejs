5eadfd54f3483eb2fb28e9998b64206ef4fd22a1
Scheuer <rodrigo.scheuer@hotmail.com>
2020-04-08 09:52:06 -0300

    Add route validations

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const cors = require("cors");
-const routes = require("./routes");
+const cors = require('cors');
+const routes = require('./routes');
+const { errors } = require('celebrate');
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(cors());
 
 app.use(express.json()); // deixar antes das rotas
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,3 +1,4 @@
+const { celebrate, Segments, Joi } = require('celebrate');
 const express = require('express');
 const routes = express.Router();
 
@@ -8,11 +9,34 @@ const SessionController = require('./controllers/SessionController');
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.list);
-routes.post('/ongs', OngController.create);
-routes.get('/ongs/incidents', OngController.listIncidents);
+routes.get('/ongs/incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown()
+}), OngController.listIncidents);
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().length(2)
+    })
+}), OngController.create);
 
 routes.get('/incidents', IncidentController.list);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.post('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.create);
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete);
 
 module.exports = routes;
